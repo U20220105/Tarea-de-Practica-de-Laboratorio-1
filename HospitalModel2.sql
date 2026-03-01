3e9ffb08ceedcfc1f3e44af1cb7541f3453a2c21
U20220105 <148370915+U20220105@users.noreply.github.com>
2026-03-01 15:12:37 -0600

    Add files via upload

---
+++ HospitalModel2.sql
@@ -0,0 +1,14 @@
+USE Hospital2;
+GO
+
+-- Ver categorías
+SELECT * FROM StaffCategoryModels;
+
+-- Ver especialidades
+SELECT * FROM SpecialityModels;
+
+-- Ver personal
+SELECT * FROM StaffModels;
+
+-- Ver pacientes
+SELECT * FROM PatientModels;
